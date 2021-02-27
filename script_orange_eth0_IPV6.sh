e02547922c55a8cfdf9e783d78e5859195be1006
Mikael R <61910581+shisva@users.noreply.github.com>
2021-02-27 14:41:44 +0100

    Ajout templates
    Ajout de templates pour config IPV4 & IPV6

--- script_orange_eth0_IPV6.sh
+++ script_orange_eth0_IPV6.sh
@@ -3,50 +3,6 @@ source /opt/vyatta/etc/functions/script-template
 
 configure
 
-set firewall ipv6-name WAN_IN-6 default-action drop
-set firewall ipv6-name WAN_IN-6 description "packets from internet to intranet"
-
-set firewall ipv6-name WAN_IN-6 rule 3001 action accept
-set firewall ipv6-name WAN_IN-6 rule 3001 state established enable
-set firewall ipv6-name WAN_IN-6 rule 3001 state related enable
-set firewall ipv6-name WAN_IN-6 rule 3001 description "allow established/related sessions"
-
-set firewall ipv6-name WAN_IN-6 rule 3002 action drop
-set firewall ipv6-name WAN_IN-6 rule 3002 state invalid enable
-set firewall ipv6-name WAN_IN-6 rule 3002 description "drop Invalid state"
-
-set firewall ipv6-name WAN_IN-6 rule 3003 action accept
-set firewall ipv6-name WAN_IN-6 rule 3003 protocol icmpv6
-set firewall ipv6-name WAN_IN-6 rule 3003 description "allow ICMPv6"
-
-set firewall ipv6-name WAN_LOCAL-6 default-action drop
-set firewall ipv6-name WAN_LOCAL-6 description "packets from internet to gateway"
-
-set firewall ipv6-name WAN_LOCAL-6 rule 3001 action accept
-set firewall ipv6-name WAN_LOCAL-6 rule 3001 state established enable
-set firewall ipv6-name WAN_LOCAL-6 rule 3001 state related enable
-set firewall ipv6-name WAN_LOCAL-6 rule 3001 description "allow established/related sessions"
-
-set firewall ipv6-name WAN_LOCAL-6 rule 3002 action drop
-set firewall ipv6-name WAN_LOCAL-6 rule 3002 state invalid enable
-set firewall ipv6-name WAN_LOCAL-6 rule 3002 description "drop Invalid state"
-
-set firewall ipv6-name WAN_LOCAL-6 rule 3003 action accept
-set firewall ipv6-name WAN_LOCAL-6 rule 3003 protocol icmpv6
-set firewall ipv6-name WAN_LOCAL-6 rule 3003 description "allow ICMPv6"
-
-set firewall ipv6-name WAN_LOCAL-6 rule 3004 description "allow DHCPv6 client/server"
-set firewall ipv6-name WAN_LOCAL-6 rule 3004 action accept
-set firewall ipv6-name WAN_LOCAL-6 rule 3004 destination port 546
-set firewall ipv6-name WAN_LOCAL-6 rule 3004 protocol udp
-set firewall ipv6-name WAN_LOCAL-6 rule 3004 source port 547  
-
-set firewall ipv6-name WAN_OUT-6 default-action accept
-set firewall ipv6-name WAN_OUT-6 description "packets to internet"
-
-set interfaces ethernet eth0 vif 832 firewall in ipv6-name WAN_IN-6
-set interfaces ethernet eth0 vif 832 firewall local ipv6-name WAN_LOCAL-6
-set interfaces ethernet eth0 vif 832 firewall out ipv6-name WAN_OUT-6
 set interfaces ethernet eth0 vif 832 ipv6 address autoconf
 set interfaces ethernet eth0 vif 832 ipv6 dup-addr-detect-transmits 1
 
